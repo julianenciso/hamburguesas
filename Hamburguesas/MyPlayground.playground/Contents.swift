581664a790b4c5c79af32483a7d3ed98fd765cc4
julianenciso <julian@enciso.org>
2016-11-06 23:45:20 -0500

    Update Contents.swift

--- Hamburguesas/MyPlayground.playground/Contents.swift
+++ Hamburguesas/MyPlayground.playground/Contents.swift
@@ -4,6 +4,6 @@ import UIKit
 
 var str = "Hello, playground"
 
-let color = UIColor(red:30/255, green: 100/255, blue: 20/255, alpha: 1)
+let color = UIColor(red:240/255, green: 160/255, blue: 0/255, alpha: 1)
 
 arc4random() % 100
